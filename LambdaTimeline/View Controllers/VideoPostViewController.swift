0c6be825d63b86d9f5f0db9336abd4d52405d559
Conner Alegre <cga0@pm.me>
2018-10-17 14:03:31 -0600

    Add base storyboard and code for Video Posts

---
+++ LambdaTimeline/View Controllers/VideoPostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VideoPostViewController.swift
+//  LambdaTimeline
+//
+//  Created by Conner on 10/17/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class VideoPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
